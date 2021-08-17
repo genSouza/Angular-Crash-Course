486b5222adc29d74b742f052fc657b8dee02bdf6
Genilton Souza <genilton.souza26@gmail.com>
2021-08-17 16:52:06 -0400

    Crud: get, update and delete

--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -5,16 +5,25 @@ import { Task } from 'src/app/Task';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-
   tasks: Task[] = [];
 
-  constructor(private service: TaskService) { }
+  constructor(private service: TaskService) {}
 
   ngOnInit(): void {
-    this.tasks = this.service.getTasks();
+    this.service.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
   }
 
+  deleteTask(task: Task) {
+    this.service.deleteTask(task).subscribe(() => this.ngOnInit());
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.service.updateTaskReminder(task).subscribe();
+  }
 }
