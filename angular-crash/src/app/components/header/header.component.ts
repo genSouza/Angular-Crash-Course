02b4528a0aeb03e52691b967c4717d2bd2ed732d
Genilton Souza <genilton.souza26@gmail.com>
2021-08-17 18:23:02 -0400

    limits component to route

--- angular-crash/src/app/components/header/header.component.ts
+++ angular-crash/src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { UiService } from './../../services/ui.service';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,7 +12,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = false;
   subscription!: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService
       .onToggle()
       .subscribe((value) => (this.showAddTask = value));
@@ -22,5 +22,9 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask() {
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
